7e686d8d8864479db9a5f7b0d3ac32fa02e22d8e
OOCLDM\lisa8 <lisa8@oocl.com>
2020-07-22 18:00:29 +0800

    [OO Step by Step][Sam Li] feature : 1.create Student, Teacher class 2. implement introduce function and introduceWith function

--- src/main/java/practice07/Student.java
+++ src/main/java/practice07/Student.java
@@ -1,4 +1,30 @@
 package practice07;
 
-public class Student {
+public class Student extends Person {
+
+    private Klass klass;
+
+    public Student(String name, int age) {
+        super(name, age);
+    }
+
+    public Student(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        return String.format("%s I am a Student. I am at %s.", super.introduce(), klass.getDisplayName());
+    }
+
+
 }
