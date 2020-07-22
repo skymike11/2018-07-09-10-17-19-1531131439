9d9f80ca2fb888a0a007ba77cf60a1e52aa6ca5b
skymike <skymike11@qq.com>
2020-07-22 20:24:11 +0800

    [OO Step by Step][Sam Li] feature : 1. update Student's introduce in order to check is class Leader 2. add id field in Teacher

--- src/main/java/practice08/Teacher.java
+++ src/main/java/practice08/Teacher.java
@@ -1,4 +1,30 @@
 package practice08;
 
-public class Teacher {
+public class Teacher extends Person {
+    private Klass klass;
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public Teacher(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public String introduceWith(Student student) {
+        String result = " I am a Teacher. " + (student.getKlass().getNumber() == klass.getNumber() ?
+                String.format("I teach %s.", student.getName()) : String.format("I don't teach %s.", student.getName()));
+        return super.introduce() + result;
+    }
+
+    @Override
+    public String introduce() {
+        return String.format("%s I am a Teacher. I teach %s.", super.introduce(), klass == null ? "No Class" : klass.getDisplayName());
+    }
+
 }
