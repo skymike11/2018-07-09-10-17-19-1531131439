22fc92d79c2475c91b67163e0d231855ceb225c6
skymike <skymike11@qq.com>
2020-07-22 20:22:14 +0800

    [OO Step by Step][Sam Li] feature : 1.create Klass and add leader function 2. add id field and override equals in Person

--- src/main/java/practice08/Person.java
+++ src/main/java/practice08/Person.java
@@ -1,4 +1,42 @@
 package practice08;
 
 public class Person {
+
+    private int id;
+
+    protected String name;
+
+    protected int age;
+
+    public Person(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public String introduce() {
+        return String.format("My name is %s. I am %d years old.", name, age);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Person target = (Person) obj;
+        return (this.id == target.id) && (this.age == target.age) && (this.name == target.name);
+    }
 }
