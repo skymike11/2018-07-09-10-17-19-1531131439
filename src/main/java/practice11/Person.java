6e560985ba16b4de8ea70bbc925302b3487bf317
skymike <skymike11@qq.com>
2020-07-22 21:45:46 +0800

    [OO Step by Step][Sam Li] feature :test11 1. perpare basic Class

--- src/main/java/practice11/Person.java
+++ src/main/java/practice11/Person.java
@@ -1,4 +1,48 @@
 package practice11;
 
 public class Person {
+
+    protected int id;
+
+    protected String name;
+
+    protected int age;
+
+    public Person(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public String introduce() {
+        return String.format("My name is %s. I am %d years old.", name, age);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null)
+            return false;
+        Person target = (Person) obj;
+        return this.id == target.id;
+    }
 }
